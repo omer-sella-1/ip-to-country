e877c11763c54308ae80f0fd210a21360b5646ac
Omer Sella <omerse@jfrog.com>
2025-08-31 16:06:18 +0300

    Changed the max bucket number to be env variable

--- src/models/database.py
+++ src/models/database.py
@@ -3,17 +3,23 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from src.config import Config
 
+# generates engine that connects to the database file specified in the config
 engine = create_engine(
     f"sqlite:///{Config.IP_DATABASE_FILE.replace('.csv', '.db')}",
     echo=False,  # Set to True for SQL debugging
 )
 
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Create the base class for declarative class definitions
 Base = declarative_base()
 
 
+# This method provides a session to interact with the database
+# establish an actual connection to the DB and return a session object
 def get_db_session():
+    # create a new session
     session = SessionLocal()
     try:
         return session
@@ -22,5 +28,6 @@ def get_db_session():
         raise
 
 
+# this function initializes the database by creating all tables defined in the models
 def init_database():
     Base.metadata.create_all(bind=engine)
--- src/models/ip_location.py
+++ src/models/ip_location.py
@@ -6,9 +6,14 @@ from src.models.database import Base
 
 
 class IPLocation(Base):
-
+    # __tablename__ is a reserve word in SQLAlchemy to define the table name
     __tablename__ = "ip_locations"
 
+    # columns definitions. nullable=False means the column cannot be null
+    # index=True creates an index on the column to speed up queries
+    # BigInteger is used for large integers (like IP addresses in integer form)
+    # String(length) defines a string column with a maximum length
+    # Index is used to create a composite index on multiple columns
     id = Column(Integer, primary_key=True)
     start_ip_int = Column(BigInteger, nullable=False, index=True)
     end_ip_int = Column(BigInteger, nullable=False, index=True)
@@ -17,12 +22,16 @@ class IPLocation(Base):
     country = Column(String(100), nullable=False)
     city = Column(String(100), nullable=False)
 
+    # __table_args__ allows for additional table arguments, like indexes and other constraints
     __table_args__ = (Index("idx_ip_range", "start_ip_int", "end_ip_int"),)
 
+    # static method to convert an IP address string to its integer representation
+    # used for searching and storing IP addresses efficiently
     @staticmethod
     def ip_to_int(ip_str: str) -> int:
         return int(ip_address(ip_str))
 
+    # method to set the IP range and automatically compute their integer representations
     def set_ip_range(self, start_ip: str, end_ip: str) -> None:
         self.start_ip_str = start_ip
         self.end_ip_str = end_ip
